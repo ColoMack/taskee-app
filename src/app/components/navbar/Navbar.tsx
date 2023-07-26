915b76ca6fafa9a97d1918017a2087e880a66c27
ColoMack <colomack6@gmail.com>
2023-07-26 21:41:32 +0300

    few changes and added some new files..

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+
+import Container from "../Container";
+import Logo from "./Logo";
+
+const Navbar = () => {
+    return (
+        <div className="background">
+            <div className="sideA">
+                the sidebar
+            </div>
+            <div className="sideB">
+                the content area
+            </div>
+        </div>
+    );
+}
+
+export default Navbar
